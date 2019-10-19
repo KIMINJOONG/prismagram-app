153752ca6b59504a84ba1c4e877ba4a643ae8ae1
KIMINJOONG <ijboym216@gmail.com>
2019-10-19 12:38:15 +0900

    12.1 Auth Components part One

---
+++ components/AuthInput.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Container = styled.View`
+    margin-bottom: 10px;
+`;
+
+const TextInput = styled.TextInput`
+    
+`;
+const AuthInput = ({
+    placeholder, 
+    value, 
+    autoCapitalize,
+    keyboardType = 'default',
+
+}) => (
+    <Container>
+        <TextInput 
+            keyboardType={keyboardType}
+            autoCapitalize={autoCapitalize}
+            placeholder={placeholder} 
+            value={value} />
+    </Container>
+
+);
+
+AuthInput.propTypes = {
+    placeholder: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    keyboardType: PropTypes.oneOf([
+        'default', 
+        'number-pad', 
+        'decimal-pad', 
+        'numeric', 
+        'email-address',
+        'phone-pad'
+    ]),
+    autoCapitalize: PropTypes.oneOf([
+        ''
+    ])
+
+}
+
+export default AuthInput;
