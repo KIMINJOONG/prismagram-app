1f96cc3ed30aeac829038aac37cc987967c042f9
KIMINJOONG <ijboym216@gmail.com>
2019-10-20 23:43:14 +0900

    12.7 Sign up

--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -1,9 +1,10 @@
-import React, {useState }from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { TouchableWithoutFeedback, Keyboard } from "react-native";
 import AuthButton from "../../components/AuthButton";
 import AuthInput from "../../components/AuthInput";
 import useInput from "../../hooks/useInput";
-import {Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { Alert } from "react-native";
 import { useMutation } from "react-apollo-hooks";
 import { LOG_IN } from "./AuthQueries";
 
@@ -13,65 +14,57 @@ const View = styled.View`
   flex: 1;
 `;
 
-const Text = styled.Text``;
-
-export default ({navigation}) => {
-  const emailInput = useInput('');
+export default ({ navigation }) => {
+  const emailInput = useInput("");
   const [loading, setLoading] = useState(false);
   const [requestSecretMutation] = useMutation(LOG_IN, {
     variables: {
       email: emailInput.value
     }
-  })
-  const handleLogin = async() => {
+  });
+  const handleLogin = async () => {
     const { value } = emailInput;
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(value === '') {
-      return Alert.alert('이메일을 입력해주세요.');
-    } else if(!value.includes('@') || !value.includes('.')) {
-      return Alert.alert('올바른 이메일 형식을 입력해주세요');
-    } else if(!emailRegex.test(value)) {
-      return Alert.alert('해당 이메일은 유효하지않습니다.');
+    if (value === "") {
+      return Alert.alert("Email can't be empty");
+    } else if (!value.includes("@") || !value.includes(".")) {
+      return Alert.alert("Please write an email");
+    } else if (!emailRegex.test(value)) {
+      return Alert.alert("That email is invalid");
     }
-
     try {
       setLoading(true);
       const {
         data: { requestSecret }
       } = await requestSecretMutation();
-      if(requestSecret) {
-        Alert.alert('비밀번호를 이메일로 보냈습니다.');
-        navigation.navigate('Confirm', { email: value });
+      if (requestSecret) {
+        Alert.alert("Check your email");
+        navigation.navigate("Confirm", { email: value });
         return;
       } else {
-        Alert.alert('계정을 찾을수 없습니다.');
-        navigation.navigate('Signup', { email: value });
+        Alert.alert("Account not found");
+        navigation.navigate("Signup", { email: value });
       }
-      
-    }catch(e) {
-      Alert.alert('지금 로그인을 할 수 없습니다.');
-    }finally {
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Can't log in now");
+    } finally {
       setLoading(false);
     }
-  }
+  };
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View>
-        <AuthInput 
-          {...emailInput} 
-          placeholder="Email" 
-          keyboardType="email-address" 
-          returnKeyType='send'
+        <AuthInput
+          {...emailInput}
+          placeholder="Email"
+          keyboardType="email-address"
+          returnKeyType="send"
           onSubmitEditing={handleLogin}
           autoCorrect={false}
-          />
-        <AuthButton 
-          loading={loading}
-          onPress={handleLogin} 
-          text="Log In" 
         />
+        <AuthButton loading={loading} onPress={handleLogin} text="Log In" />
       </View>
     </TouchableWithoutFeedback>
-    
-  )
-}
+  );
+};
