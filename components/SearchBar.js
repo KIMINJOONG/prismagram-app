e4d168b8cc7c74d687345ae3413f8e1a1d12cddd
KIMINJOONG <ijboym216@gmail.com>
2019-10-25 21:42:28 +0900

    change color

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,39 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import { TextInput } from 'react-native-gesture-handler';
-import constants from '../constants';
-import styles from '../styles';
+import React from "react";
+import { TextInput } from "react-native";
+import PropTypes from "prop-types";
+import constants from "../constants";
+import styles from "../styles";
 
-const SearchBar = ({
-    onChange,
-    value,
-    onSubmit
-}) => {
-    return (
-        <TextInput
-            style={{ 
-                width: constants.width - 40 , 
-                height: 35, 
-                backgroundColor: styles.darkGreyColor,
-                padding: 10,
-                borderRadius: 5,
-                textAlign: 'center',
-            }}
-            returnKeyType='search'
-            onChangeText={onChange}
-            onEndEditing={onSubmit}
-            value={value} 
-            placeholder={'Search'} 
-            placeholderTextColor={styles.darkGreyColor}
-        />
-    )
-};
+const SearchBar = ({ onChange, value, onSubmit }) => (
+  <TextInput
+    style={{
+      width: constants.width - 40,
+      height: 35,
+      backgroundColor: styles.lightGreyColor,
+      padding: 10,
+      borderRadius: 5,
+      textAlign: "center"
+    }}
+    returnKeyType="search"
+    onChangeText={onChange}
+    onEndEditing={onSubmit}
+    value={value}
+    placeholder={"Search"}
+    placeholderTextColor={styles.darkGreyColor}
+  />
+);
 
 SearchBar.propTypes = {
-    onChange: PropTypes.func.isRequired,
-    value: PropTypes.string.isRequired,
-    onSubmit: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
 };
-
 export default SearchBar;
