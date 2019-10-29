a55cd4040d0bd3b1724b988bf76fa56ce0d1d0af
KIMINJOONG <ijboym216@gmail.com>
2019-10-29 09:01:14 +0900

    18.5 Saving Photo

--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { Camera } from "expo-camera";
 import constants from "../../constants";
@@ -7,15 +7,46 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import { Ionicons } from '@expo/vector-icons';
 import { Platform } from "@unimodules/core";
 import styles from "../../styles";
+import * as MediaLibrary from 'expo-media-library';
 
 const View = styled.View`
   flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Icon = styled.View``;
+
+const Button = styled.View`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  border: 10px solid ${styles.lightGreyColor};
 `;
 
 export default ({ navigation }) => {
+  const caemraRef = useRef();
+  const [canTakePhoto, setCanTakePhoto] = useState(true);
   const [loading, setLoading] = useState(true);
   const [hasPermission, setHasPermission] = useState(false);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const takePhoto = async() => {
+    if(!canTakePhoto) {
+      return;
+    }
+    try {
+      setCanTakePhoto(false);
+      const { uri } = await caemraRef.takePictureAsync({
+        quality: 1,
+      });
+      const asset = await MediaLibrary.createAssetAsync(uri);  
+    }catch(e) {
+      console.log(e);
+      setCanTakePhoto(true);
+    }
+    
+    
+  }
 
   const askPermission = async () => {
     try {
@@ -46,22 +77,30 @@ export default ({ navigation }) => {
       {loading ? (
         <Loader />
       ) : hasPermission ? (
-        <Camera
-          type={cameraType}
-          style={{ 
-            justifyContent: 'flex-end',
-            padding: 15,
-            width: constants.width, 
-            height: constants.height / 2
-          }}
-        >
+        <>
+          <Camera
+            ref={caemraRef}
+            type={cameraType}
+            style={{ 
+              justifyContent: 'flex-end',
+              padding: 15,
+              width: constants.width, 
+              height: constants.height / 2
+            }}
+          >
 
-          <TouchableOpacity onPress={toggleType}>
-            <View>
-              <Ionicons name={Platform.OS === 'ios' ? 'ios-reverse-camera' : 'md-reverse-camera'} size={28} color={styles.blackColor} />
-            </View>
-          </TouchableOpacity>
-        </Camera>
+            <TouchableOpacity onPress={toggleType}>
+              <View>
+                <Ionicons name={Platform.OS === 'ios' ? 'ios-reverse-camera' : 'md-reverse-camera'} size={28} color={styles.blackColor} />
+              </View>
+            </TouchableOpacity>
+          </Camera>
+          <View>
+            <TouchableOpacity onPress={takePhoto} disabled={!canTakePhoto}>
+              <Button />
+            </TouchableOpacity>
+          </View>
+        </>
       ) : null}
     </View>
   );
